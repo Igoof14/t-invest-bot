aa9870b3ab64baa504470773b8474587d37e6b33
Aleksej Goof <aleksejgoof@example.com>
2025-10-14 16:46:59 +0300

    Refactor module imports and service references

--- app/bot.py
+++ app/bot.py
@@ -6,7 +6,9 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler  # type: ignore
 from apscheduler.triggers.cron import CronTrigger  # type: ignore
 from core.config import config
 from core.enums import ReportType
-from handlers import base
+from handlers.registration import register_handlers
+from services.report_service import ReportService
+from utils.bot_utils import BotUtils
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,18 +18,18 @@ dp = Dispatcher()
 
 async def main():
     """Start the bot."""
-    base.register_handlers(dp, bot)
-    await base.set_commands(bot)
+    register_handlers(dp, bot)
+    await BotUtils.set_commands(bot)
     scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
 
     scheduler.add_job(
-        base.send_report,
+        ReportService.send_report,
         CronTrigger(hour=18, minute=10),
         kwargs={"bot": bot, "report_type": ReportType.DAILY},
     )
 
     scheduler.add_job(
-        base.send_report,
+        ReportService.send_report,
         CronTrigger(day_of_week="fri", hour=18, minute=10, second=1),
         kwargs={"bot": bot, "report_type": ReportType.WEEKLY},
     )
