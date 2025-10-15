d8736d1f17b22513f9a0b99e9edd953618b33d62
Aleksej Goof <aleksejgoof@example.com>
2025-10-15 11:58:54 +0300

    Add invest module with coupon payment functions

---
+++ app/invest/invest.py
@@ -0,0 +1,56 @@
+import os
+from datetime import datetime
+
+from dotenv import load_dotenv
+from tinkoff.invest import Client, MoneyValue, OperationType, exceptions
+
+_ = load_dotenv(".env")
+TOKEN = str(os.environ.get("tInvest"))
+
+
+def cast_money(v: MoneyValue):
+    """Cast MoneyValue to float."""
+    return v.units + v.nano / 1e9
+
+
+def get_coupon_payment(start_datetime: datetime):
+    """Get payment amount for today."""
+    with Client(TOKEN) as client:
+        accounts = client.users.get_accounts()
+
+        total_amount = 0
+        message = ""
+        for account in accounts.accounts:
+            operations = client.operations.get_operations(
+                account_id=account.id, from_=start_datetime
+            )
+
+            account_amount = 0
+            if not operations.operations:
+                message += f"<b>{account.name}</b>: 0₽\n"
+                continue
+
+            for operation in operations.operations:
+                if operation.operation_type == OperationType.OPERATION_TYPE_COUPON:
+                    operation_amount = cast_money(operation.payment)
+                    account_amount += operation_amount
+
+            total_amount += account_amount
+            message += f"<b>{account.name}</b>: {account_amount:,.2f}₽\n"
+
+        message += f"\n<b>Сумма выплат:</b> {total_amount:,.2f}₽"
+
+        return message
+
+
+def check_token(token: str) -> bool:
+    """Проверяет, что токен действителен."""
+    try:
+        with Client(token) as client:
+            _ = client.users.get_info()
+            ...
+        return True
+
+    except Exception as e:
+        print(f"Ошибка при проверке токена: {e}")
+        return False
